325e07c8c383e9015796b56550527faf4d8aea3e
Aitor García Luiz <aitorgarcialuiz@gmail.com>
2018-02-19 19:16:17 +0100

    Preparación del proyecto
    Preparación del proyecto en Visual Studio 2017 partiendo de la base TDI.

---
+++ Source/TDI.cpp
@@ -0,0 +1,14 @@
+#include <C_Matrix.hpp>
+//#include <C_Image.hpp>
+//#include <stdio.h>   // printf
+//#include <stdlib.h>  // atof (pasar el argumento a double)
+//#include <math.h>    // senos y cosenos, pow y sqrt
+
+int Test(int argc, char **argv);
+int main(int argc, char **argv)
+{
+	printf("Prueba\n");
+	system("Pause");
+
+	//return Test(argc, argv);
+}
