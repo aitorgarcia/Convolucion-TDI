eceb904e9b0a2eff2c28440a259937c097b4511e
Aitor García Luiz <aitorgarcialuiz@gmail.com>
2018-03-07 13:42:34 +0100

    Añadido nucleo del método convolución

--- Source/TDI.cpp
+++ Source/TDI.cpp
@@ -4,6 +4,7 @@
 #include <C_General.hpp>
 
 void Convolution();
+void TratamientoBordes();
 
 int main(int argc, char **argv)
 {
@@ -17,82 +18,73 @@ int main(int argc, char **argv)
 	C_Image imagenEntrada;
 	imagenEntrada.ReadBMP("Hercules_Gris.bmp");
 
+	//C_Matrix matrizConvolucion(0,2,0,2,-1);
+	//matrizConvolucion(1, 1) = 8;
 
-	C_Matrix matrizConvolucion(-1,1,-1,1,-1);
+	C_Matrix matrizConvolucion(-1, 1, -1, 1, -1);
 	matrizConvolucion(0, 0) = 8;
 
 	matrizConvolucion.Print(matrizConvolucion.ColN(), (int)C_DOUBLE_P_INF);
 
 
 
-
-	int sumaConvolucion;
-
-
-	C_Image imagenSalida;
-	imagenSalida = imagenEntrada;
-
-	
-	printf("\n\nAplicando cambios...\n");
-
-	int imagenInicio = imagenEntrada.FirstRow();
-	int imagenFinal = imagenEntrada.LastRow();
-	int imagenInicio2 = imagenEntrada.FirstCol();
-	int imagenFinal2= imagenEntrada.LastCol();
-
-	int imagenInicio3 = matrizConvolucion.FirstRow();
-	int imagenFinal3 = matrizConvolucion.LastRow();
-	int imagenInicio4 = matrizConvolucion.FirstCol();
-	int imagenFinal4 = matrizConvolucion.LastCol();
+	C_Image imagenSalida = imagenEntrada;
+	//imagenSalida.Print(imagenSalida.ColN(), (int)C_DOUBLE_P_INF);
 
 
-	printf("Entra\n");
-	for (int i = imagenInicio; i <= imagenFinal; i++) {
-		for (int j = imagenInicio2; j <= imagenFinal2; j++) {
-			printf("Entra imagen\n");
+	double sumaConvolucion;
+	int rango = matrizConvolucion.LastRow();
+
+
+	for (int x = imagenEntrada.FirstRow()+1; x <= imagenEntrada.LastRow()-1; x++) {
+		for (int y = imagenEntrada.FirstCol()+1; y <= imagenEntrada.LastCol()-1; y++) {
+
 			sumaConvolucion = 0;
-			for (int x = imagenInicio3; x <= imagenFinal3; x++) {
-				for (int y = imagenInicio4; y <= imagenFinal4; y++) {
-					printf("Entra matriz\n");
-					sumaConvolucion = sumaConvolucion + (int)((int)imagenEntrada(i, j) * (int)matrizConvolucion(x, y));
-					printf("Sale matriz\n");
+			
+			for (int i = matrizConvolucion.FirstRow(); i <= matrizConvolucion.LastRow(); i++) {
+				for (int j = matrizConvolucion.FirstRow(); j <= matrizConvolucion.LastRow(); j++) {
+
+					int comienzo = matrizConvolucion.LastRow() / 2;
+					int xCalc = (x - comienzo) + i;
+					int yCalc = (y - comienzo) + j;
+
+					sumaConvolucion = sumaConvolucion + (matrizConvolucion(i, j) *  imagenEntrada(xCalc, yCalc));
 				}
 			}
-			printf("Sale imagen\n");
-			imagenSalida(i, j) = sumaConvolucion;
-			printf("Suma\n");
+
+			imagenSalida(x, y) = sumaConvolucion;
 		}
 	}
-	printf("Cambios aplicados.\n\n");
-	
 
-	
-
-		
 
 
 	//Usar el constructor de la libreria Matrix para crear la matriz de convolución y que tenga los indices -1,0,1
 	//	MoveSubMat? para mover la matriz de convolucion
 	//	Trunc? para llevar los valores al límite del rango
+
+	
+	//imagenSalida.Convolution(imagenEntrada, matrizConvolucion);
+
 	
 	imagenSalida.Trunc(0, 255);
 
 	//Una vez hecha la convolucion hacer una comprobación del flag fail
-
-
-
 	//Hay que tener en cuenta la paleta de colores cuando se guarde la imagen en el disco.
 	
 	
-	imagenSalida.WriteBMP("Hercules_Gris2.bmp");
+	imagenSalida.WriteBMP("Hercules_Gris3.bmp");
 
 
 	system("Pause");
 }
 
 
-/*
+
 void Convolution(){
 	
 }
-*/
+
+
+void TratamientoBordes() {
+
+}
