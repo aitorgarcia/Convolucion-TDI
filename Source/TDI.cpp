fc96ac2b60ca685f7dd21f8a2d88486f51c38705
Aitor García Luiz <aitorgarcialuiz@gmail.com>
2018-03-12 02:31:52 +0100

    Add método reajuste de valores

--- Source/TDI.cpp
+++ Source/TDI.cpp
@@ -3,10 +3,11 @@
 #include <C_Matrix.hpp>
 #include <C_General.hpp>
 
-void Convolution();
-int CalcularValorPixel(int x, int y, int i, int j, C_Matrix matrizConvolucion);
-
 C_Image imagenEntrada;
+C_Image imagenSalida;
+
+int CalcularValorPixel(int x, int y, int i, int j, C_Matrix matrizConvolucion);
+void ReajusteValores();
 
 int main(int argc, char **argv)
 {
@@ -17,17 +18,22 @@ int main(int argc, char **argv)
 	printf("#                                          Aitor Garcia Luiz   #\n");
 	printf("################################################################\n\n");
 
-	imagenEntrada.ReadBMP("lena512.bmp");
+	//Lectura de la imagen de entrada:
+	imagenEntrada.ReadBMP(argv[1]);
 
+	//Creación matriz de convolución:
 	C_Matrix matrizConvolucion(-1, 1, -1, 1, -1);
 	matrizConvolucion(0, 0) = 9;
 	matrizConvolucion.Print(matrizConvolucion.ColN(), (int)C_DOUBLE_P_INF);
 
+	/*C_Matrix matrizConvolucion(-2, 2, -2, 2, -1);
+	matrizConvolucion(0, 0) = 9;
+	matrizConvolucion.Print(matrizConvolucion.ColN(), (int)C_DOUBLE_P_INF);*/
+
+	imagenSalida = imagenEntrada;
 
 
-	C_Image imagenSalida = imagenEntrada;
-	//imagenSalida.Print(imagenSalida.ColN(), (int)C_DOUBLE_P_INF);
-
+	//Cálculo de la convolución:
 	int sumaConvolucion;
 
 	for (int x = imagenEntrada.FirstRow(); x <= imagenEntrada.LastRow(); x++) {
@@ -44,30 +50,17 @@ int main(int argc, char **argv)
 			imagenSalida(x, y) = sumaConvolucion;
 		}
 	}
-
-
-
-	//Usar el constructor de la libreria Matrix para crear la matriz de convolución y que tenga los indices -1,0,1
-	//	MoveSubMat? para mover la matriz de convolucion
-	//	Trunc? para llevar los valores al límite del rango
-
-	
 	//imagenSalida.Convolution(imagenEntrada, matrizConvolucion);
 
+	//Reajuste de valores menores de 0 o mayores de 255 para que estén dentro del rango (0,255):
+	ReajusteValores();
 	
-	imagenSalida.Trunc(0, 255);
-
-	//Una vez hecha la convolucion hacer una comprobación del flag fail
-	//Hay que tener en cuenta la paleta de colores cuando se guarde la imagen en el disco.
-	
-	imagenSalida.WriteBMP("Hercules_Gris3.bmp");
+	//Escritura de la imagen de salida:
+	imagenSalida.WriteBMP("convolution_image.bmp");
 
 	system("Pause");
 }
 
-void Convolution(){
-	
-}
 
 int CalcularValorPixel(int x, int y, int i, int j, C_Matrix matrizConvolucion) {
 
@@ -82,37 +75,48 @@ int CalcularValorPixel(int x, int y, int i, int j, C_Matrix matrizConvolucion) {
 	
 	// 1.Esquina superior izquierda:
 	if (xConv < imagenEntrada.FirstRow() && yConv < imagenEntrada.FirstCol())
-		return matrizConvolucion(i, j) * imagenEntrada(imagenEntrada.FirstRow(), imagenEntrada.FirstCol());
+		return (int)matrizConvolucion(i, j) * (int)imagenEntrada(imagenEntrada.FirstRow(), imagenEntrada.FirstCol());
 
 	// 2.Esquina superior derecha:
 	else if(xConv < imagenEntrada.FirstRow() && yConv > imagenEntrada.LastCol())
-		return matrizConvolucion(i, j) * imagenEntrada(imagenEntrada.FirstRow(), imagenEntrada.LastCol());
+		return (int)matrizConvolucion(i, j) * (int)imagenEntrada(imagenEntrada.FirstRow(), imagenEntrada.LastCol());
 
 	// 3.Esquina inferior izquierda:
 	else if (xConv > imagenEntrada.LastRow() && yConv < imagenEntrada.FirstCol())
-		return matrizConvolucion(i, j) * imagenEntrada(imagenEntrada.LastRow(), imagenEntrada.FirstCol());
+		return (int)matrizConvolucion(i, j) * (int)imagenEntrada(imagenEntrada.LastRow(), imagenEntrada.FirstCol());
 
 	// 4.Esquina inferior derecha:
 	else if (xConv > imagenEntrada.LastRow() && yConv > imagenEntrada.LastCol())
-		return matrizConvolucion(i, j) * imagenEntrada(imagenEntrada.LastRow(), imagenEntrada.LastCol());
+		return (int)matrizConvolucion(i, j) * (int)imagenEntrada(imagenEntrada.LastRow(), imagenEntrada.LastCol());
 
 	// 5.Lado superior:
 	else if (xConv < imagenEntrada.FirstRow())
-		return matrizConvolucion(i, j) * imagenEntrada(imagenEntrada.FirstRow(), yConv);
+		return (int)matrizConvolucion(i, j) * (int)imagenEntrada(imagenEntrada.FirstRow(), yConv);
 
 	// 6.Lado izquierdo:
 	else if (yConv < imagenEntrada.FirstCol())
-		return matrizConvolucion(i, j) * imagenEntrada(xConv, imagenEntrada.FirstCol());
+		return (int)matrizConvolucion(i, j) * (int)imagenEntrada(xConv, imagenEntrada.FirstCol());
 
 	// 7.Lado inferior:
 	else if (xConv > imagenEntrada.LastRow())
-		return matrizConvolucion(i, j) * imagenEntrada(imagenEntrada.LastRow(), yConv);
+		return (int)matrizConvolucion(i, j) * (int)imagenEntrada(imagenEntrada.LastRow(), yConv);
 
 	// 8.Lado derecho:
 	else if (yConv > imagenEntrada.LastCol())
-		return matrizConvolucion(i, j) * imagenEntrada(xConv, imagenEntrada.LastCol());
+		return (int)matrizConvolucion(i, j) * (int)imagenEntrada(xConv, imagenEntrada.LastCol());
 
 	// 9.Por defecto sin tratamiento de bordes:
-	return  matrizConvolucion(i, j) *  imagenEntrada(xConv, yConv);
+	return  (int)matrizConvolucion(i, j) *  (int)imagenEntrada(xConv, yConv);
 }
 
+void ReajusteValores() {
+
+	for (int x = imagenSalida.FirstRow(); x <= imagenSalida.LastRow(); x++) {
+		for (int y = imagenSalida.FirstCol(); y <= imagenSalida.LastCol(); y++) {
+			if (imagenSalida(x, y) < 0)
+				imagenSalida(x, y) = 0;
+			else if (imagenSalida(x, y) > 255)
+				imagenSalida(x, y) = 255;
+		}
+	}
+}
